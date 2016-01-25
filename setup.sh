d40ed57118c085c7e236b6ac10c8cdddcb7523dd
Dries Vints <dries.vints@gmail.com>
2016-01-25 22:26:03 +0100

    Update setup

--- setup.sh
+++ setup.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
+
 # Install dotfiles on a fresh system
-
-# Install Homebrew
 source ./homebrew/setup.sh
-
-# Install OS X utilities
 source ./osx/setup.sh
-
-# Install apps
 source ./apps/setup.sh
-
-# Install fonts
 source ./fonts/setup.sh
