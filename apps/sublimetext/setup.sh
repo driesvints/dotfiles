bf66fb9b7195701225712c318a1a38adca40ba67
Dries Vints <dries.vints@gmail.com>
2016-01-29 20:02:06 +0100

    Small changes

--- apps/sublimetext/setup.sh
+++ apps/sublimetext/setup.sh
@@ -2,6 +2,10 @@
 
 echo "Setting up Sublime Text 3..."
 
+# Make sure library directory exists
+mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Packages
+mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages
+
 # Copy user preferences
 ln -s ~/.dotfiles/apps/sublimetext/preferences/ ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
 
