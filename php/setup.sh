ad65ff00c2ba863c9062feba4e84a5d53dfd807b
Dries Vints <dries.vints@gmail.com>
2016-02-15 21:24:34 +0100

    Remove Phan

--- php/setup.sh
+++ php/setup.sh
@@ -2,7 +2,7 @@
 
 echo "Setting up PHP..."
 
-# Install PHP 7 (Will be activated through .path)
+# Install PHP 7
 brew install homebrew/php/php70
 
 # Install Composer
@@ -10,4 +10,4 @@ curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
 
 # Install global packages
-/usr/local/bin/composer require laravel/installer laravel/lumen-installer etsy/phan
+/usr/local/bin/composer require laravel/installer laravel/lumen-installer
