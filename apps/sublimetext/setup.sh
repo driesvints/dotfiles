7a29a49e9c5ffe14ee017bce01056ccfe79b01e4
Dries Vints <dries@beatswitch.com>
2016-02-16 15:39:25 +0100

    Remove slash from directory symlink

--- apps/sublimetext/setup.sh
+++ apps/sublimetext/setup.sh
@@ -7,7 +7,7 @@ mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Packages
 mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages
 
 # Symlink user preferences
-ln -s ~/.dotfiles/apps/sublimetext/preferences/ ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+ln -s ~/.dotfiles/apps/sublimetext/preferences ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
 
 # Install Package Control
 curl -o ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages/Package\ Control.sublime-package https://packagecontrol.io/Package\ Control.sublime-package
