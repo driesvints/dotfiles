d6042e54317a6efaaa539366e3e86a90a667802a
Dries Vints <dries.vints@gmail.com>
2016-03-02 21:45:13 +0100

    Consolidate app install files

--- apps/install.sh
+++ apps/install.sh
@@ -2,8 +2,14 @@
 
 echo "Setting up apps..."
 
-# Link Alfred
-brew cask alfred link
-
 # Setup PhpStorm
 # source $DOTFILES/apps/phpstorm/install.sh
+
+# Install Package Control for Sublime Text 3
+mkdir -p $LIBRARY/Application\ Support/Sublime\ Text\ 3/Installed\ Packages
+curl -o $LIBRARY/Application\ Support/Sublime\ Text\ 3/Installed\ Packages/Package\ Control.sublime-package https://packagecontrol.io/Package\ Control.sublime-package
+
+# Symlink PhpStorm Themes
+mkdir -p $LIBRARY/Preferences/WebIde100/colors
+ln -s $DOTFILES/app/phpstorm/themes/Material\ Peacock\ Custom.icls $LIBRARY/Preferences/WebIde100/colors/Material\ Peacock\ Custom.icls
+
--- apps/phpstorm/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo "Setting up PHPStorm..."
-
-# Symlink Themes
-ln -s $DOTFILES/app/phpstorm/themes/Material\ Peacock\ Custom.icls $LIBRARY/Preferences/WebIde100/colors/Material\ Peacock\ Custom.icls
