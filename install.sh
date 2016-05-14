7eaec506affb87f799b4a3884e0406f61288969d
Dries Vints <dries.vints@gmail.com>
2016-05-14 12:52:59 +0200

    Add Laravel Valet

--- install.sh
+++ install.sh
@@ -22,7 +22,10 @@ curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
 
 # Install global Composer packages
-/usr/local/bin/composer global require laravel/installer laravel/lumen-installer tightenco/jigsaw
+/usr/local/bin/composer global require laravel/installer laravel/lumen-installer laravel/valet tightenco/jigsaw
+
+# Install Laravel Valet
+valet install
 
 # Create a Sites directory
 # This is a default directory for OS X user accounts but doesn't comes pre-installed
