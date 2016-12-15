a12e692b0f1297321d1ba42df89812567060517d
Dries Vints <dries.vints@gmail.com>
2016-12-15 15:53:37 +0100

    Add Mixed Content Scan package

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
 
 # Install global Composer packages
-/usr/local/bin/composer global require laravel/installer laravel/lumen-installer laravel/valet tightenco/jigsaw spatie/http-status-check
+/usr/local/bin/composer global require laravel/installer laravel/lumen-installer laravel/valet tightenco/jigsaw spatie/http-status-check bramus/mixed-content-scan
 
 # Install Laravel Valet
 $HOME/.composer/vendor/bin/valet install
