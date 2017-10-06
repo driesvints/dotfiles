e03d8df7aa4b2dc5b8ea0906b8b54561839812be
Dries Vints <dries.vints@gmail.com>
2017-10-06 13:34:10 +0200

    Remove unused Composer binaries

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
 
 # Install global Composer packages
-/usr/local/bin/composer global require laravel/installer laravel/lumen-installer laravel/valet tightenco/jigsaw spatie/http-status-check bramus/mixed-content-scan laravel/spark-installer
+/usr/local/bin/composer global require laravel/installer laravel/lumen-installer laravel/spark-installer laravel/valet tightenco/jigsaw
 
 # Install Laravel Valet
 $HOME/.composer/vendor/bin/valet install
