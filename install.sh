b77fd40ce62ba79f42d46f2f75bcfb82b3d47277
Dries Vints <dries.vints@gmail.com>
2017-12-27 20:27:02 +0100

    Remove Jigsaw

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
 
 # Install global Composer packages
-/usr/local/bin/composer global require laravel/installer laravel/lumen-installer laravel/spark-installer laravel/valet tightenco/jigsaw
+/usr/local/bin/composer global require laravel/installer laravel/lumen-installer laravel/spark-installer laravel/valet
 
 # Install Laravel Valet
 $HOME/.composer/vendor/bin/valet install
