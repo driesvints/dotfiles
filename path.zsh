72e47171f46b83a714a9cdba2b81a344c7e46c9c
Dries Vints <dries.vints@gmail.com>
2018-03-29 18:50:53 +0200

    Remove link for Homebrew PHP path
    This isn't needed anymore because Homebrew now links this as a symlink to /usr/local/bin.

--- path.zsh
+++ path.zsh
@@ -4,9 +4,6 @@ export PATH="$HOME/.node/bin:$PATH"
 # Use project specific binaries before global ones
 export PATH="node_modules/.bin:vendor/bin:$PATH"
 
-# Make sure PHP 7.2 is loaded
-export PATH="$(brew --prefix homebrew/php/php72)/bin:$PATH"
-
 # Make sure coreutils are loaded before system commands
 # I've disabled this for now because I only use "ls" which is
 # referenced in my aliases.zsh file directly.
