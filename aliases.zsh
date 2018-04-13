9d10919ea5a964b4c47d09d99952c6273aaf9578
Dries Vints <dries.vints@gmail.com>
2018-04-13 11:32:04 +0200

    Add Docker aliases

--- aliases.zsh
+++ aliases.zsh
@@ -22,3 +22,10 @@ alias vhalt="vagrant halt"
 alias vssh="vagrant ssh"
 alias vreload="vagrant reload"
 alias vrebuild="vagrant destroy --force && vagrant up"
+
+# Docker
+alias dstop="docker stop $(docker ps -a -q)"
+alias dpurgecontainers="dstop && docker rm $(docker ps -a -q)"
+alias dpurgeimages="docker rmi $(docker images -q)"
+dbuild() { docker build -t=$1 .; }
+dbash() { docker exec -it $(docker ps -aqf "name=$1") bash; }
