86fc6bf9dbd60214913c4f5b4e5b199d602e09a1
Kevin Krauss <earlkrauss@gmail.com>
2018-06-20 17:04:15 -0700

    update for linux

--- install.sh
+++ install.sh
@@ -1,32 +1,44 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "Setting up your Mac..."
+echo "Setting up your Computer..."
 
-# Check for Homebrew and install if we don't have it
-if test ! $(which brew); then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if ! [ -d "$HOME/.rvm" ]; then
+  gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+  \curl -sSL https://get.rvm.io | bash -s stable --ruby
 fi
 
-ln -s $HOME/.dotfiles/system/bash_profile $HOME/.bash_profile
+if ! [ -d "$HOME/.nvm" ]; then
+  \curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+  nvm install node
+fi
+
+# Check for Homebrew and install if we don't have it
+if ! [ -x "$(command -v brew)" ]; then
+  if [ "$(uname)" == "Darwin" ]; then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  else
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+    sudo apt install linuxbrew-wrapper
+  fi
+fi
+
+ln -s $HOME/.dotfiles/system/bash_profile $HOME/.bash_profile || echo ".bash_profile exists"
 
 # install bash-it
 # git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
-ln -s $HOME/.dotfiles/bash_it/themes/powerline-plus /home/kevin/.bash_it/themes/powerline-plus
+ln -s $HOME/.dotfiles/bash_it/themes/powerline-plus /home/kevin/.bash_it/themes/powerline-plus || echo "powerline-plus exists"
 
 
 # Update Homebrew recipes
 brew update
 brew install ansible
 
-mkdir $HOME/Projects
-cd $HOME/Projects
-git clone https://github.com/earlonrails/superlumic-config.git
-cd superlumic-config
-ansible-playbook -i "localhost," config/$USER.yml
-
+pushd ansible-config
+ansible-playbook -i "localhost," $USER.yml
+popd
 # Create a Sites directory
 # This is a default directory for macOS user accounts but doesn't comes pre-installed
-mkdir $HOME/Sites
+mkdir $HOME/Sites || echo "Sites exists"
 
 # Set macOS preferences
 # We will run this last because this will reload the shell
--- readme.md
+++ readme.md
@@ -7,3 +7,11 @@ git clone git@github.com:earlonrails/dotfiles.git $HOME/.dotfiles
 cd $HOME/.dotfiles
 source install.sh
 ```
+
+
+https://ubuntu.pkgs.org/16.04/ubuntu-universe-amd64/caffeine_2.8.3-3build1_all.deb.html
+
+http://c7be0123d7efff32860a-a5a4fb8b39b86d00a1eb7d52603ae1d2.r6.cf1.rackcdn.com/vpn-unlimited_4.22-amd64_001_release_STANDALONE.deb
+
+http://freelinuxtutorials.com/quick-tips-and-tricks/quick-tip-install-fingerprint-scanner-fprint-ubuntu-16-04-linux/
+
