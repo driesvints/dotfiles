01c028a82d03df827b0b8b2a00b6743e1991b31f
Dries Vints <dries.vints@gmail.com>
2018-11-06 14:21:00 +0100

    Disable docker aliases

--- aliases.zsh
+++ aliases.zsh
@@ -26,11 +26,11 @@ alias vreload="vagrant reload"
 alias vrebuild="vagrant destroy --force && vagrant up"
 
 # Docker
-alias dstop="docker stop $(docker ps -a -q)"
-alias dpurgecontainers="dstop && docker rm $(docker ps -a -q)"
-alias dpurgeimages="docker rmi $(docker images -q)"
-dbuild() { docker build -t=$1 .; }
-dbash() { docker exec -it $(docker ps -aqf "name=$1") bash; }
+#alias dstop="docker stop $(docker ps -a -q)"
+#alias dpurgecontainers="dstop && docker rm $(docker ps -a -q)"
+#alias dpurgeimages="docker rmi $(docker images -q)"
+#dbuild() { docker build -t=$1 .; }
+#dbash() { docker exec -it $(docker ps -aqf "name=$1") bash; }
 
 # Git
 alias commit="git add . && git commit -m"
