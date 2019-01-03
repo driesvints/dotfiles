205e7c40a7da02072bac757f3066445d05287859
Dries Vints <dries.vints@gmail.com>
2019-01-03 16:41:03 +0100

    Remove custom bin directory

--- path.zsh
+++ path.zsh
@@ -14,6 +14,3 @@ export PATH="node_modules/.bin:vendor/bin:$PATH"
 
 # Local bin directories before anything else
 export PATH="/usr/local/bin:/usr/local/sbin:$PATH"
-
-# Load custom commands
-export PATH="$DOTFILES/bin:$PATH"
