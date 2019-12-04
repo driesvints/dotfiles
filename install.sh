2cc9947a41bec444a79c588bd5ed5be46052dcff
Dries Vints <dries.vints@gmail.com>
2019-12-04 16:52:40 +0100

    Add step to set MySQL default root password

--- install.sh
+++ install.sh
@@ -14,6 +14,9 @@ brew update
 brew tap homebrew/bundle
 brew bundle
 
+# Set default MySQL root password and auth type.
+mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY 'password'; FLUSH PRIVILEGES;"
+
 # Install PHP extensions with PECL
 pecl install memcached imagick
 
