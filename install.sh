4643c59422f055019b4d4e8965903ed17b923c3e
Alan Yee <alanyee@users.noreply.github.com>
2020-02-13 11:27:14 -0800

    Update install.sh
    Turn off google analytics

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ echo "Setting up your Mac..."
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  brew analytics off
 fi
 
 # Update Homebrew recipes
