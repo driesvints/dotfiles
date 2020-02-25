f6f15d5b49fc6af730418bbf142a9688c6eb3d64
Dries Vints <dries.vints@gmail.com>
2020-02-25 20:25:44 +0100

    Update aliases

--- aliases.zsh
+++ aliases.zsh
@@ -1,23 +1,22 @@
 # Shortcuts
 alias copyssh="pbcopy < $HOME/.ssh/id_rsa.pub"
-alias reloadcli="source $HOME/.zshrc"
+alias reloadshell="source $HOME/.zshrc"
 alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
-alias ll="/usr/local/opt/coreutils/libexec/gnubin/ls -ahlF --color --group-directories-first"
-weather() { curl -4 wttr.in/${1:-antwerp} }
+alias ll="/usr/local/opt/coreutils/libexec/gnubin/ls -AhlFo --color --group-directories-first"
 alias phpstorm='open -a /Applications/PhpStorm.app "`pwd`"'
 alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
 alias c="clear"
-alias zbundle="antibody bundle < $DOTFILES/zsh_plugins.txt > $DOTFILES/zsh_plugins.sh"
 
 # Directories
 alias dotfiles="cd $DOTFILES"
 alias library="cd $HOME/Library"
 alias sites="cd $HOME/Sites"
-alias lara="sites && cd laravel/"
+alias laravel="sites && cd laravel/"
 
 # Laravel
 alias a="php artisan"
-alias ams="php artisan migrate:fresh --seed"
+alias fresh="php artisan migrate:fresh --seed"
+alias seed="php artisan db:seed"
 
 # PHP
 alias php73="/usr/local/Cellar/php@7.3/7.3.13/bin/php"
