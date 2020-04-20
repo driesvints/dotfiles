8d5bb53bdc3cdef5514b7aea69ba3b4ae1489441
Haikal Saadh <spam.haikal@gmail.com>
2020-04-20 20:33:32 +1000

    Remove backup and macos config from install.

--- install.sh
+++ install.sh
@@ -17,10 +17,3 @@ brew bundle
 # Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
 rm -rf $HOME/.zshrc
 ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
-
-# Symlink the Mackup config file to the home directory
-ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
-
-# Set macOS preferences
-# We will run this last because this will reload the shell
-source .macos
