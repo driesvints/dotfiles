766f2ffce0553ba4602d480fedb29f704f0984bf
Dries Vints <dries@vints.io>
2020-05-03 11:52:01 +0200

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -3,6 +3,7 @@
 echo "Cloning repositories..."
 
 SITES=$HOME/Sites
+BLADE=$SITES/blade-ui-kit
 LARAVEL=$SITES/laravel
 
 # Personal
@@ -16,6 +17,13 @@ git clone git@github.com:fullstackbelgium/fullstackbelgium.be.git $SITES/fullsta
 git clone git@github.com:fullstackeurope/fullstackeurope.com.git $SITES/fullstackeurope.com
 git clone git@github.com:laravelio/laravel.io.git $SITES/laravel.io
 git clone git@github.com:laravelio/paste.laravel.io.git $SITES/paste.laravel.io
+
+# Blade UI Kit
+git clone git@github.com:blade-ui-kit/blade-heroicons.git $BLADE/blade-heroicons
+git clone git@github.com:blade-ui-kit/blade-icons.git $BLADE/blade-icons
+git clone git@github.com:blade-ui-kit/blade-ui-kit.git $BLADE/blade-ui-kit
+git clone git@github.com:blade-ui-kit/blade-ui-kit.com.git $BLADE/blade-ui-kit.com
+git clone git@github.com:blade-ui-kit/blade-zondicons.git $BLADE/blade-zondicons
 
 # Laravel
 git clone git@github.com:laravel/browser-kit-testing.git $LARAVEL/browser-kit-testing
