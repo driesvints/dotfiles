cb4117672dd97d562363befe5dd45883808020af
Dries Vints <dries.vints@gmail.com>
2020-08-16 12:27:50 +0200

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -19,6 +19,7 @@ git clone git@github.com:laravelio/paste.laravel.io.git $SITES/paste.laravel.io
 
 # Blade UI Kit
 git clone git@github.com:blade-ui-kit/art.git $BLADE/art
+git clone git@github.com:blade-ui-kit/awesome-tall-stack.git $BLADE/awesome-tall-stack
 git clone git@github.com:blade-ui-kit/blade-docs.git $BLADE/blade-docs
 git clone git@github.com:blade-ui-kit/blade-heroicons.git $BLADE/blade-heroicons
 git clone git@github.com:blade-ui-kit/blade-icons.git $BLADE/blade-icons
