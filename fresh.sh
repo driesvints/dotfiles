2c23674271f09c083052c9ed179796f119e24666
Mark van den Broek <mvdnbrk@gmail.com>
2020-09-20 13:16:09 +0200

    Update the Homebrew installer command
    Resolves #41

--- fresh.sh
+++ fresh.sh
@@ -4,7 +4,7 @@ echo "Setting up your Mac..."
 
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
 # Update Homebrew recipes
