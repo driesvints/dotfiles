7b0b1c3826e92ef404dce1a8704cad3a3514f9ec
Dries Vints <dries@vints.io>
2020-09-26 11:33:15 +0200

    Add composer alias

--- aliases.zsh
+++ aliases.zsh
@@ -23,6 +23,7 @@ alias seed="php artisan db:seed"
 alias cfresh="rm -rf vendor/ composer.lock && composer i"
 alias php74="docker run -it -w /data -v ${PWD}:/data:delegated --entrypoint php --rm registry.gitlab.com/grahamcampbell/php:7.4"
 alias php8="docker run -it -w /data -v ${PWD}:/data:delegated --entrypoint php --rm registry.gitlab.com/grahamcampbell/php:8.0"
+alias composer="php -d memory_limit=-1 /usr/local/bin/composer"
 
 # JS
 alias nfresh="rm -rf node_modules/ package-lock.json && npm install"
