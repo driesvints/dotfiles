0f5792df7fa6eeaad200ba79ee8fa96e75bf6789
Michaël De Boey <info@michaeldeboey.be>
2021-02-28 00:05:36 +0100

    Update fresh.sh

--- fresh.sh
+++ fresh.sh
@@ -4,7 +4,7 @@ echo "Setting up your Mac..."
 
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 # Update Homebrew recipes
