6d2593f8ed0e311d636806274dd566e63c190098
Dries Vints <dries@vints.io>
2021-03-29 10:08:38 +0200

    Revert "feat(ssh.sh): bypass passphrase prompt (#63)"
    This reverts commit ef5136d43750bd41ed57100f28818f709d773e62.

--- ssh.sh
+++ ssh.sh
@@ -4,7 +4,7 @@ echo "Generating a new SSH key for GitHub..."
 
 # Generating a new SSH key
 # https://docs.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent#generating-a-new-ssh-key
-ssh-keygen -t ed25519 -C $1 -f ~/.ssh/id_ed25519 -P $2
+ssh-keygen -t ed25519 -C $1 -f ~/.ssh/id_ed25519
 
 # Adding your SSH key to the ssh-agent
 # https://docs.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent#adding-your-ssh-key-to-the-ssh-agent
