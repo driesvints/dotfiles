cf7d613282264e316fc2b6762af2e7cf78b9c43e
Tim Mortimer <tim@kiteframe.co.uk>
2021-04-29 18:26:07 +0100

    Correct SSH public key name

--- aliases.zsh
+++ aliases.zsh
@@ -1,5 +1,5 @@
 # Shortcuts
-alias copyssh="pbcopy < $HOME/.ssh/id_rsa.pub"
+alias copyssh="pbcopy < $HOME/.ssh/id_ed25519.pub"
 alias reloadshell="source $HOME/.zshrc"
 alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
 alias ll="/usr/local/opt/coreutils/libexec/gnubin/ls -AhlFo --color --group-directories-first"
