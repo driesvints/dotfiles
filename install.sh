048753e561327f8315488d43c24e5f6d85d96e77
Dries Vints <dries@vints.io>
2021-05-07 13:56:55 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ brew update
 brew tap homebrew/bundle
 brew bundle
 
-# Set default MySQL root password and auth type.
+# Set default MySQL root password and auth type
 mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY 'password'; FLUSH PRIVILEGES;"
 
 # Install PHP extensions with PECL
@@ -49,6 +49,5 @@ ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 # Symlink the Mackup config file to the home directory
 ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
 
-# Set macOS preferences
-# We will run this last because this will reload the shell
+# Set macOS preferences - we will run this last because this will reload the shell
 source .macos
