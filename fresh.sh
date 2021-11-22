84ba1d9079637e8ee04e0ec5fd7fe37e085a1f6e
Dries Vints <dries@vints.io>
2021-11-22 15:51:41 +0100

    Update fresh.sh

--- fresh.sh
+++ fresh.sh
@@ -10,6 +10,9 @@ fi
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
 # Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
