662ac075d6d18442d5cd88df5e15b5ac943c8a70
Dries Vints <dries@vints.io>
2021-12-07 17:38:46 +0100

    Update aliases.zsh

--- aliases.zsh
+++ aliases.zsh
@@ -6,6 +6,8 @@ alias ll="/opt/homebrew/opt/coreutils/libexec/gnubin/ls -AhlFo --color --group-d
 alias phpstorm='open -a /Applications/PhpStorm.app "`pwd`"'
 alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
 alias c="clear"
+alias compile="commit 'compile'"
+alias version="commit 'version'"
 
 # Directories
 alias dotfiles="cd $DOTFILES"
