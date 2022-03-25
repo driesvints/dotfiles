954ac46da7b8998438eb8cf1755da8dbe7152d52
Dries Vints <dries@vints.io>
2022-03-25 13:03:23 +0100

    Update fresh.sh

--- fresh.sh
+++ fresh.sh
@@ -33,7 +33,7 @@ mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_passwor
 pecl install imagick redis swoole
 
 # Install global Composer packages
-/usr/local/bin/composer global require laravel/installer laravel/valet beyondcode/expose spatie/global-ray
+/usr/local/bin/composer global require laravel/installer laravel/valet beyondcode/expose spatie/global-ray spatie/visit
 
 # Install Laravel Valet
 $HOME/.composer/vendor/bin/valet install
