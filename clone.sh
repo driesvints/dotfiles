9cb034fd964c624642d40bfebd4fcf9ba407c678
Dries Vints <dries@vints.be>
2024-02-13 14:30:25 +0100

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -12,15 +12,15 @@ git clone git@github.com:blade-ui-kit/blade-ui-kit.com.git $SITES/blade-ui-kit.c
 git clone git@github.com:laravel/blog.laravel.com.git $SITES/blog.laravel.com
 git clone git@github.com:blade-ui-kit/demo.git $SITES/demo.blade-ui-kit.com
 git clone git@github.com:driesvints/driesvints.com.git $SITES/driesvints.com
-git clone git@github.com:laravel/envoyer.git $SITES/envoyer.laravel.com
+git clone git@github.com:laravel/envoyer.git $SITES/envoyer
 git clone git@github.com:eventyio/eventy.io.git $SITES/eventy.io
-git clone git@github.com:laravel/forge.git $SITES/forge.laravel.com
+git clone git@github.com:laravel/forge.git $SITES/forge
 git clone git@github.com:fullstackeurope/fullstackeurope.com.git $SITES/fullstackeurope.com
 git clone git@github.com:laravel/laravel.com.git $SITES/laravel.com
 git clone git@github.com:laravelio/laravel.io.git $SITES/laravel.io
 git clone git@github.com:laravelio/paste.laravel.io.git $SITES/paste.laravel.io
 git clone git@github.com:laravel/spark.laravel.com.git $SITES/spark.laravel.com
-git clone git@github.com:laravel/vapor.git $SITES/vapor.laravel.com
+git clone git@github.com:laravel/vapor.git $SITES/vapor
 
 # Personal
 git clone git@github.com:lmsqueezy/laravel.git $CODE/lmsqueezy-laravel
