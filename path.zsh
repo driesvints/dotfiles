f810296999486506a87ae18e84431506953b7c7c
Dries Vints <dries@vints.be>
2024-04-08 10:23:17 +0200

    wip

--- path.zsh
+++ path.zsh
@@ -1,6 +1,6 @@
 # Add directories to the PATH and prevent to add the same directory multiple times upon shell reload.
 add_to_path() {
-  if [[ -d "$1" ]] && [[ ":$PATH:" != *":$1:"* ]] && [[ ":$PATH:" != "$1:"* ]]; then
+  if [[ -d "$1" ]] && [[ ":$PATH:" != *":$1:"* ]]; then
     export PATH="$1:$PATH"
   fi
 }
