d4dc93d6079ddf551223fabe99ce998b90409b33
Evangelos Lamprou <vagos@lamprou.xyz>
2026-08-25 11:21:44 -0400

    Quote bootstrap paths

--- fresh.sh
+++ fresh.sh
@@ -11,21 +11,21 @@ else
 fi
 
 # Check for Oh My Zsh and install if we don't have it
-if test ! $(which omz); then
+if ! command -v omz >/dev/null 2>&1; then
   /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
 fi
 
 # Check for Homebrew and install if we don't have it
-if test ! $(which brew); then
+if ! command -v brew >/dev/null 2>&1; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> "$HOME/.zprofile"
   eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
 # Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
-rm -rf $HOME/.zshrc
-ln -sw $HOME/.dotfiles/.zshrc $HOME/.zshrc
+rm -rf "$HOME/.zshrc"
+ln -sw "$HOME/.dotfiles/.zshrc" "$HOME/.zshrc"
 
 # Update Homebrew recipes
 brew update
@@ -38,18 +38,18 @@ brew bundle --file ./Brewfile
 mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY 'password'; FLUSH PRIVILEGES;"
 
 # Create a projects directories
-mkdir $HOME/Code
-mkdir $HOME/Herd
+mkdir "$HOME/Code"
+mkdir "$HOME/Herd"
 
 # Create Code subdirectories
-mkdir $HOME/Code/blade-ui-kit
-mkdir $HOME/Code/laravel
+mkdir "$HOME/Code/blade-ui-kit"
+mkdir "$HOME/Code/laravel"
 
 # Clone Github repositories
 ./clone.sh
 
 # Symlink the Mackup config file to the home directory
-ln -s ./.mackup.cfg $HOME/.mackup.cfg
+ln -s ./.mackup.cfg "$HOME/.mackup.cfg"
 
 # Set macOS preferences - we will run this last because this will reload the shell
 source ./.macos
